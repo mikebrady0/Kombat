c0dd9cb3fd4b9c693b4f94d075de75b08eecda2d
mikebrady0 <147462552+mikebrady0@users.noreply.github.com>
2025-02-24 15:44:26 -0500

    supabase url keys initialized in supabaseclient.js

--- kombat/src/app/utils/supabaseClient.js
+++ kombat/src/app/utils/supabaseClient.js
@@ -1,5 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
+NEXT_PUBLIC_SUPABASE_URL=https://jhupzpkoiogtbssbpnfo.supabase.co
+NEXT_PUBLIC_SUPABASE_ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpodXB6cGtvaW9ndGJzc2JwbmZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkzNzc2MjcsImV4cCI6MjA1NDk1MzYyN30.ZiWgDgIvk4k1tup9Px_8Ti33rxjr_owNY9ghBdr186A
+            
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
